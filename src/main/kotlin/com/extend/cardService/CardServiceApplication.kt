b26e4b4a14bb5ac233295fd227c2db6cca764801
vachistol <chisto.victoria2@gmail.com>
2022-10-27 22:26:51 -0400

    Create CardServiceApplication.kt

---
+++ src/main/kotlin/com/extend/cardService/CardServiceApplication.kt
@@ -0,0 +1,11 @@
+package com.extend.cardService
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CardServiceApplication
+
+fun main(args: Array<String>) {
+	runApplication<CardServiceApplication>(*args)
+}
