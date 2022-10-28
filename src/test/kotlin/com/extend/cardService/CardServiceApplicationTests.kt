d3da52935e9d8bfbd091b45e52ea5581d58b4918
vachistol <chisto.victoria2@gmail.com>
2022-10-27 22:31:11 -0400

    Create CardServiceApplicationTests.kt

---
+++ src/test/kotlin/com/extend/cardService/CardServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.extend.cardService
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CardServiceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
